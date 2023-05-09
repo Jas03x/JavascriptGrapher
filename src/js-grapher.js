59cf3e586a44983c18a55a817b3d45b44b23a0bd
Jas <jas03x@gmail.com>
2023-05-09 18:45:06 -0400

    c

--- src/js-grapher.js
+++ src/js-grapher.js
@@ -2,8 +2,11 @@
 function draw_graph(canvas_id, file)
 {
     const canvas = document.getElementById(canvas_id);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
     const ctx = canvas.getContext("2d");
 
     ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 150, 100);
+    ctx.fillRect(10, 10, 1000, 1000);
 }
