b761785d09960188719dff107027d581e0c3ca56
Jas <jas03x@gmail.com>
2023-05-09 21:31:47 -0400

    c

--- src/js-grapher.js
+++ src/js-grapher.js
@@ -1,12 +1,37 @@
 
+class vec2
+{
+    constructor(x = 0.0, y = 0.0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 function draw_graph(canvas_id, file)
 {
+    let width  = window.innerWidth;
+    let height = window.innerHeight;
+    let radius = 100.0;
+
     const canvas = document.getElementById(canvas_id);
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
 
     const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 1000, 1000);
+    const point_array = [
+        new vec2((width / 2) - radius, (height / 2) - radius),
+        new vec2((width / 2) + radius, (height / 2) - radius),
+        new vec2((width / 2) + radius, (height / 2) + radius),
+        new vec2((width / 2) - radius, (height / 2) + radius)
+    ];
+
+    ctx.beginPath();
+    ctx.moveTo(point_array[0].x, point_array[0].y);
+    ctx.lineTo(point_array[1].x, point_array[1].y);
+    ctx.lineTo(point_array[2].x, point_array[2].y);
+    ctx.lineTo(point_array[3].x, point_array[3].y);
+    ctx.lineTo(point_array[0].x, point_array[0].y);
+    ctx.stroke();
 }
